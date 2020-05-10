6bd60cd0eeb728db272d8a89569e61ed48fb088f
Barış Mutlu <mutlubaris_1@hotmail.com>
2020-05-10 13:19:57 +0300

    Initial commit

---
+++ Inventory Packages/RunOverPickup.cs
@@ -0,0 +1,18 @@
+﻿using GameDevTV.Inventories;
+using UnityEngine;
+
+namespace InventoryExample.Control
+{
+    [RequireComponent(typeof(Pickup))]
+    public class RunOverPickup : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other) 
+        {
+            var player = GameObject.FindGameObjectWithTag("Player");
+            if (other.gameObject == player)
+            {
+                GetComponent<Pickup>().PickupItem();
+            }
+        }
+    }
+}
